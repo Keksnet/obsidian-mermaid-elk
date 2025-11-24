0c144d779098a0e15fd948e845e1b1936e9a762b
Marlon Pohl <admin@neo8.de>
2025-11-24 16:35:20 +0100

    Rename main class

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import {loadMermaid, Plugin} from 'obsidian';
 import elkLayouts from "@mermaid-js/layout-elk";
 
-export default class MyPlugin extends Plugin {
+export default class MermaidElk extends Plugin {
 
 	async onload() {
 		const globalMermaid = await loadMermaid();
